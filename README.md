02c4ffb162af94514383ce4ca72e367460b1c0ba
Christopher Buenaventura <christopherbuenaventura@gmail.com>
2018-05-09 21:24:25 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Post Me
+
+Post my handle to a specific picture of me on Instagram at 20 to 65 second
+intervals.
+
+## Usage
+
+    node index.js [csrfToken] [sessionId]
---
+++ index.js
@@ -0,0 +1,31 @@
+let request = require("request")
+
+let csrfToken = process.argv[2]
+let sessionId = process.argv[3]
+
+let cookie = [
+  `csrftoken=${csrfToken}`, // required, changes per session
+  `sessionid=${sessionId}` // required, changes per session
+]
+
+let options = {
+  method: "POST",
+  body: "comment_text=%40cgbuen",
+  url: "https://www.instagram.com/web/comments/1775702115170110573/add/",
+  headers: {
+    'cookie': cookie.join(";"), // required
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36', // probably should have
+    'x-csrftoken': csrfToken, // required
+    'content-type': 'application/x-www-form-urlencoded', // required
+    'referer': 'https://www.instagram.com/p/Bikj6ppn8xt/' // required
+  }
+}
+
+function callback(err, res, body) {
+  console.log(body)
+  console.log((new Date()).toUTCString())
+}
+
+setInterval(function() {
+  request(options, callback)
+}, (Math.random() * 55 + 20)*1000)
